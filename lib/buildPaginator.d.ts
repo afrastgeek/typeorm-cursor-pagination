30b7dcad65f652bb2b7c2f972f0785cc5cdd71aa
Ammar Fadhlur Rahman <afrastgeek@gmail.com>
2023-01-31 08:59:16 +0700

    chore: add build result

---
+++ lib/buildPaginator.d.ts
@@ -0,0 +1,16 @@
+import { ObjectLiteral, ObjectType } from 'typeorm';
+import Paginator, { Order } from './Paginator';
+export interface PagingQuery {
+    afterCursor?: string;
+    beforeCursor?: string;
+    includeCursor?: boolean;
+    limit?: number;
+    order?: Order | 'ASC' | 'DESC';
+}
+export interface PaginationOptions<Entity> {
+    entity: ObjectType<Entity>;
+    alias?: string;
+    query?: PagingQuery;
+    paginationKeys?: Extract<keyof Entity, string>[];
+}
+export declare function buildPaginator<Entity extends ObjectLiteral>(options: PaginationOptions<Entity>): Paginator<Entity>;
